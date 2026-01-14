3f5999b6f3b4c63bf099fb3629118b7967df8100
webcapa <254017788+webcapa@users.noreply.github.com>
2026-01-13 21:51:12 -0500

    added history card

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //const API = "http://127.0.0.1:8000";
 const API = "https://webcapaapi.azurewebsites.net/api";
 let algorithms = {};
+let history = [];
 
 async function loadAlgorithms() {
   const res = await fetch(`${API}/algorithms`);
@@ -36,12 +37,21 @@ async function computeFromText() {
 
 document.getElementById("run").addEventListener("click", async () => {
   const output = document.getElementById("output");
+  const historyEl = document.getElementById("history");
 
   try {
     const res = await computeFromText();
-    output.textContent = res.answers
+    const resultString = res.answers
                         .map((val, i) => `${val} ${res.units[i]}`)
                         .join("\n");
+
+    output.textContent = resultString;
+
+    history.push(`${res.label}\n${resultString}`);
+    if (history.length > 5) history.shift();
+
+    historyEl.textContent = history.join("\n\n");
+
   } catch (err) {
     console.log(err.message);
     output.textContent = "Something Went Wrong, Try Again";
@@ -51,6 +61,11 @@ document.getElementById("run").addEventListener("click", async () => {
   card.classList.remove("animate");
   void card.offsetWidth; // force reflow
   card.classList.add("animate");
+
+  const historyCard = document.querySelector(".history-card");
+  historyCard.classList.remove("animate");
+  void historyCard.offsetWidth; // force reflow
+  historyCard.classList.add("animate");
 
 });
 
